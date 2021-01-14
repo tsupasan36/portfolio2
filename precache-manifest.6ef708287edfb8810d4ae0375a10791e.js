d5a9dbe91e3fcdc05f185140ecce1f4c0ccbf6ba
tsupasan36 <tsupasan36@gmail.com>
2021-01-14 17:22:32 +0900

    Updates

---
+++ precache-manifest.6ef708287edfb8810d4ae0375a10791e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "788e06dc6087ac71107a28cdcbc817f4",
+    "url": "/portfolio2/index.html"
+  },
+  {
+    "revision": "e80b090c71241fae6f46",
+    "url": "/portfolio2/static/css/main.73329814.chunk.css"
+  },
+  {
+    "revision": "cddfd08082f2f28533ae",
+    "url": "/portfolio2/static/js/2.126bf728.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/portfolio2/static/js/2.126bf728.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e80b090c71241fae6f46",
+    "url": "/portfolio2/static/js/main.5f3fce45.chunk.js"
+  },
+  {
+    "revision": "5b667fff427192cd9522",
+    "url": "/portfolio2/static/js/runtime-main.543d9f4a.js"
+  }
+]);
